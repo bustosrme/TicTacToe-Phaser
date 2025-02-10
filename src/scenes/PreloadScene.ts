97e2d8836974872366ee43bb92a912b25884e4b8
Mauro Bustos Roldan <bustosrme@gmail.com>
2025-02-10 20:07:54 -0300

    change folders for assets. add new sound and fonts

--- src/scenes/PreloadScene.ts
+++ src/scenes/PreloadScene.ts
@@ -5,20 +5,21 @@ export default class PreloadScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('Background_main', 'assets/Background-main.png');
-        this.load.image('Background', 'assets/Background.png');
-        this.load.image('Board', 'assets/Board.png');
-        this.load.image('ImgX', 'assets/X.png');
-        this.load.image('ImgO', 'assets/O.png');
-        this.load.image('Home', 'assets/Home.png')
-        this.load.image('Particle', 'assets/Particle.png');
-        this.load.image('Play', 'assets/Play.png');
-        this.load.image('Reload', 'assets/Reload.png');
-        this.load.audio('Tap', 'assets/Tap.mp3');
-        this.load.audio('Win', 'assets/Win.mp3');
+        this.load.image('Background_main', 'assets/images/Background-main.png');
+        this.load.image('Background', 'assets/images/Background.png');
+        this.load.image('Board', 'assets/images/Board.png');
+        this.load.image('ImgX', 'assets/images/X.png');
+        this.load.image('ImgO', 'assets/images/O.png');
+        this.load.image('Home', 'assets/images/Home.png')
+        this.load.image('Particle', 'assets/images/Particle.png');
+        this.load.image('Play', 'assets/images/Play.png');
+        this.load.image('Reload', 'assets/images/Reload.png');
+        this.load.audio('Music', 'assets/sounds/Music.mp3');
+        this.load.audio('Tap', 'assets/sounds/Tap.mp3');
+        this.load.audio('Win', 'assets/sounds/Win.mp3');
     }
 
     create() {
-        this.scene.start('Main');
+        this.scene.start('Game');
     }
 }
